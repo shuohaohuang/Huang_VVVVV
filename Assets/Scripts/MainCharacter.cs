671c5560714452610df27905fdb6c7d5529cd858
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-10-06 20:26:25 +0200

    animaciones y escena

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -15,6 +16,8 @@ public class MainCharacter : MonoBehaviour
     private float runSpeed;
     [SerializeField]
     private float ySpeed;
+    [SerializeField]
+    private Animator animationController;
 
     private bool yAct = true;
     public static MainCharacter instance;
@@ -43,30 +46,50 @@ public class MainCharacter : MonoBehaviour
         if (horizontal != 0)
         {
             Character.position= transform.position+new Vector3(runSpeed * horizontal*0.02f,0,0);
+            Character.transform.localScale = new Vector3(horizontal, 1, 1);
         }
+        animationController.SetFloat("Speed", Mathf.Abs(runSpeed * horizontal));
+
         if (yAct)
         {
+
             if (vertical > 0)
             {
                 Character.velocity = new Vector3(0, ySpeed, 0);
                 yAct = false;
+                Debug.Log("saltando");
+                animationController.SetBool("Jump", true);
             }
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Character.gravityScale = -Character.gravityScale;
                 Character.transform.Rotate(180, 0, 0);
                 ySpeed = -ySpeed;
                 yAct = false;
+                animationController.SetBool("Falling", true);
             }
+        }
+        else
+        {
+
+            animationController.SetBool("Jump", false);
+            animationController.SetBool("Falling", true);
+
         }
         hit = Physics2D.Raycast(transform.position, -transform.up, 1);
         Debug.DrawRay(transform.position, transform.position - transform.up * 1, Color.red);
-        if(hit.collider != null)
-            yAct = true;
 
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        yAct = true;
+        animationController.SetBool("Falling", false);
+
     }
 
+    private void Run( float speed)
+    {
+
+    }
 }
